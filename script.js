ec20782cf2332e39e95a2562f8006322b3d34e0f
Hritesh1728 <hriteshalw1728.com>
2022-07-23 17:17:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Welcome to spotify clone!');
+// let songs = [
+//     {songName:'On and On',filePath:'images/On-and-On_192(PaglaSongs).mp3',coverPath:'images/On-and-on_images.jpg'},
+//     {songName:'On and On',filePath:'images/On-and-On_192(PaglaSongs).mp3',coverPath:'images/On-and-on_images.jpg'},
+//     {songName:'On and On',filePath:'images/On-and-On_192(PaglaSongs).mp3',coverPath:'images/On-and-on_images.jpg'},
+//     {songName:'On and On',filePath:'images/On-and-On_192(PaglaSongs).mp3',coverPath:'images/On-and-on_images.jpg'},
+
+// ]
+let audioElement = new Audio('images/On-and-On_192(PaglaSongs).mp3');
+audioElement.play();
